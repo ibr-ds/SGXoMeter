8d98909310ceeabdfbd6695af8d4cec58f8cdd14
Moe <mahhouk@ibr.cs.tu-bs.de>
2019-11-28 12:30:20 +0100

    script for sgx sdk benchmarks

---
+++ sgx-appsbench/sgx/runSDKCryptoBench.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for (( c=16; c<=1048576; c=c*2 ))
+do
+   ./BaselineApp -w 10 -R 60 -s$c
+done
+
+for (( c=16; c<=1048576; c=c*2 ))
+do
+   ./BenchApp -w 10 -R 60 -s$c
+done
+
+for (( c=16; c<=1048576; c=c*2 ))
+do
+   ./BenchAppSim -w 10 -R 60 -s$c
+done
+                                 
