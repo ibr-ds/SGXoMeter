76e6e354400f977ac0eefbdec6c569edb949658e
Marcel Vogt <mavogt@ssgx8.ibr.cs.tu-bs.de>
2021-07-20 19:26:44 +0200

    Weitere Module

---
+++ sgx-appsbench/sgx/test_app/OpenEnclaveSDK/OEenclave/OEtests/custom_sha256_test.c
@@ -0,0 +1,60 @@
+//
+// Created by moe on 16.07.19.
+//
+#ifdef CUSTOM_SHA256_TEST
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <openssl/sha.h>
+#include <openssl/evp.h>
+#include <UtilsStructs.h>
+
+
+#ifdef PRINT_CHECKS
+#else
+#define fprintf(stream, msg...)
+#define fflush(...)
+#endif
+
+
+static char *toHashStr;
+static globalConfig_t *globConfPtr;
+void initCustomSHA256(globalConfig_t *globalConfig)
+{
+    globConfPtr = globalConfig;
+    size_t hashStrSize = globalConfig->HASH256_LEN * sizeof(char);
+    toHashStr = (char *) malloc(hashStrSize + 1);
+    if(toHashStr == NULL)
+    {
+        fprintf(stderr, "Malloc failed!");
+    }
+    for(int i = 0; i < globalConfig->HASH256_LEN; i++)
+    {
+        toHashStr[i] = 'a';
+    }
+    toHashStr[hashStrSize] = '\0';
+}
+
+void post_SHA256_test()
+{
+    free(toHashStr);
+}
+
+int custom_SHA256_test()
+{
+    unsigned char md[SHA256_DIGEST_LENGTH];
+    fprintf(stdout, "Testing SHA-256 ");
+    if(EVP_Digest(toHashStr, globConfPtr->HASH256_LEN , md, NULL, EVP_sha256(), NULL) != 1)
+    {
+        fflush(stdout);
+        fprintf(stderr, "\nTEST SHA256 FAILED.\n");
+        return 1;
+    } else {
+        fprintf(stdout, ".");
+        fflush(stdout);
+    return 0;
+    }
+}
+
+#endif
