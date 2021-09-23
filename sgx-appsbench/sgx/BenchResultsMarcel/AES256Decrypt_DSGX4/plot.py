cd0fa88de555288c5b4bce96d3824029ea7ad876
Marcel Vogt <marcelvogt@MacBook-Pro-von-Marcel.local>
2021-09-23 17:00:59 +0200

    Graphs

--- sgx-appsbench/sgx/BenchResultsMarcel/AES256Decrypt_DSGX4/plot.py
+++ sgx-appsbench/sgx/BenchResultsMarcel/AES256Decrypt_DSGX4/plot.py
@@ -15,38 +15,38 @@ with open(fn_sgx,'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x.append(int(row[1]))
-        sgx.append(float(row[6]))
+        sgx.append(float(row[5]))
 
 with open(fn_oe,'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        oe.append(float(row[6]))
+        oe.append(float(row[5]))
 
 with open(fn_base,'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        baseline.append(float(row[6]))
+        baseline.append(float(row[5]))
 
-with open(fn_base,'r') as csvfile:
+with open(fn_graphene,'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        graphene.append(float(row[6]))
+        graphene.append(float(row[5]))
 
-plt.semilogy()
+#plt.semilogy()
 plt.semilogx(basex=2)
 #plt.xscale('log', basex=2)
 plt.plot(x,baseline,'r',label='Baseline')
 plt.plot(x,sgx,'b--',label='SGX SDK')
 plt.plot(x,oe,'g:',label='OE SDK')
-plt.plot(x,graphene,'g:',label='GrapheneSGX')
+plt.plot(x,graphene,'purple',label='GrapheneSGX')
 plt.xlabel('Decrypted Buffer Size')
 plt.xticks(x,('16 B','32 B','64 B','128 B','256 B','512 B','1 KiB','2 KiB','4 KiB','8 KiB','16 KiB','32 KiB','64 KiB','128 KiB','256 KiB','512 KiB','1 MiB'), rotation='vertical')
 #plt.vlines(32, ymin=0, ymax=baseline[0], color='0.55', linestyles='dashed',linewidth=1)
 #text(32, (10000), "optimisations", rotation=90, verticalalignment='center', size='large')
 #plt.set_minor_formatter(FormatStrFormatter('%d'))
 plt.ylabel('#Operation Per Sec')
-plt.title('SGXSDK AES GCM Decryption & Encryption on in-enclave Buffers Benchmark')
+plt.title('AES256 GCM decryption on in-enclave Buffers Benchmark')
 plt.legend()
 plt.tight_layout()
-plt.savefig("dencrypto.pdf")
+plt.savefig("AES256decrypt_DSGX4.pdf")
 #plt.show()
